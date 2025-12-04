bd65da8170f3afd27b8f06f4c5094bad34eced5d
Emil Englesson <emil@englesson.net>
2025-12-04 19:05:38 +0100

    Implemented 2025/04

---
+++ src/years/year_2025/day_04/part_2.rs
@@ -0,0 +1,77 @@
+type Grid = Vec<Vec<Cell>>;
+
+#[derive(PartialEq, Eq)]
+enum Cell {
+    Empty,
+    Paper,
+}
+
+impl From<char> for Cell {
+    fn from(value: char) -> Self {
+        match value {
+            '@' => Cell::Paper,
+            '.' => Cell::Empty,
+            chr => panic!("Unknown character '{chr}'"),
+        }
+    }
+}
+
+#[must_use]
+pub fn solve(input: &str) -> u64 {
+    let mut grid = parse(input);
+
+    std::iter::from_fn(|| {
+        let removed = remove_once(&mut grid);
+        (removed > 0).then_some(removed)
+    })
+    .sum()
+}
+
+fn remove_once(grid: &mut Grid) -> u64 {
+    let mut count = 0;
+    for x in 0..grid.len() {
+        for y in 0..grid[x].len() {
+            if grid[x][y] == Cell::Paper {
+                let neighbors = count_neighbors(grid, x, y);
+                if neighbors < 4 {
+                    grid[x][y] = Cell::Empty;
+                    count += 1;
+                }
+            }
+        }
+    }
+    count
+}
+
+fn count_neighbors(grid: &Grid, x: usize, y: usize) -> usize {
+    #[rustfmt::skip]
+    let neighbors = [
+        (-1, -1), (-1, 0), (-1, 1),
+        (0, -1),           (0, 1),
+        (1, -1),  (1, 0),  (1, 1),
+    ];
+
+    let rows = grid.len();
+    let cols = grid[0].len();
+
+    neighbors
+        .iter()
+        .filter_map(|(dx, dy)| {
+            let x = x.checked_add_signed(*dx)?;
+            let y = y.checked_add_signed(*dy)?;
+            if x < rows && y < cols {
+                Some((x, y))
+            } else {
+                None
+            }
+        })
+        .filter(|&(x, y)| grid[x][y] == Cell::Paper)
+        .count()
+}
+
+fn parse(input: &str) -> Grid {
+    input
+        .lines()
+        .map(|line| line.chars().map(Cell::from).collect())
+        .collect()
+}
